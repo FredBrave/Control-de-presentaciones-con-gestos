4e0965fab667803e175afb13a919625678d192c8
FredBrave <freddy.bravo.robalino@gmail.com>
2025-10-12 16:56:39 -0500

    RF-1: Registro de Usuario Exitoso

---
+++ presentaciones/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+  #  path('logout/', views.logoutUser, name='logout'),
+    path('', views.home, name='home')
+]
--- presentaciones/views.py
+++ presentaciones/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def registerPage(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        data = request.POST.copy()
+
+        if 'password1' in data and 'password2' not in data:
+            data['password2'] = data['password1']
+
+        form = CustomUserCreationForm(data)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Ocurrió un error durante el registro')
+
+    return render(request, 'seguridad/register.html', {'form': form})
+
+def loginPage(request):
+    return render(request, 'seguridad/login.html')
+
+def home(request):
+    return render(request, 'presentaciones/home.html')
