a4c8877495e3870b487d70460897e1b5e317df7f
FredBrave <freddy.bravo.robalino@gmail.com>
2025-10-12 15:22:15 -0500

    RF-0 feat: Agregado Modelo Usuario

--- presentaciones/models.py
+++ presentaciones/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    api_key = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+    
+    
